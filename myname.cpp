9ee31c787c7d064f592d183b34b0517ba4dbb28c
RobertJBlanchard <143756140+RobertJBlanchard@users.noreply.github.com>
2023-08-31 17:06:18 -0400

    Add files via upload

---
+++ myname.cpp
@@ -0,0 +1,12 @@
+﻿// myname.cpp : Defines the entry point for the application.
+//
+
+#include "myname.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Robert Blanchard" << endl;
+	return 0;
+}
